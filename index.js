194d462aeeba7b4fd6c88105831e892dc845773e
andre petion <contact@oli-branch.com>
2020-06-14 01:41:39 -0400

    Implemented strictEqual, test passes

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function strictEqual(num1, num2) {
     // eslint-disable-next-line no-cond-assign
     // eslint-disable-next-line eqeqeq
-    if (num1 !== num2) {
-        return true
-    } else {
+    if (num1 == num2 && typeof(num1) == typeof(num2)) {
         return false
     }
 }
