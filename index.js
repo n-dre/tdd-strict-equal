5a087e44ec243571e7c533e1474551cccf915dc2
andre petion <contact@oli-branch.com>
2020-06-14 17:18:45 -0400

    Implemented strictEqual, test passes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function strictEqual(firstValue, secondValue) {
-    // eslint-disable-next-line eqeqeq
-    if (firstValue == secondValue && typeof(firstValue) == typeof(secondValue)) {
-        return true
-    } else {
-        return false
-    }
+  // eslint-disable-next-line eqeqeq
+  if (firstValue == secondValue && typeof (firstValue) == typeof (secondValue)) {
+    return true
+  } else {
+    return false
+  }
 }
 module.exports = strictEqual
